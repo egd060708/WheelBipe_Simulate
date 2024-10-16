e780f02c68b1cf1290fa7cc98bbeed558f92285c
egd060708 <2231625449@qq.com>
2024-10-16 23:41:52 +0800

    上交模型简单部署

--- matlab/control/webots_param.m
+++ matlab/control/webots_param.m
@@ -3,13 +3,15 @@
 % 轮参数
 Mw_= 0.308;%kg
 R_= 0.07;%m
-Jw_= 387.411e-6;%kg/m^2
+Jw_= (754.296e-6);%kg/m^2
 D_= 0.54;%m
 
 % 腿部参数
 Llinks = [0.15,0.15,0.288];%无论是并联腿还是串联腿都可以用三个参数表达
 Mlinks = [0.189,0.327];
-Ilinks = [465.732e-6,2350.113e-6];
+Ilinks = [608.165e-6,2997.229e-6];
+k_ = (4*Mlinks(1)+2*Mlinks(2))/(8*(Mlinks(1)+Mlinks(2)));
+
 % Lleg = 0.3;
 % [Pleg,Mleg,Ileg] = leg_param_cal(Lleg,Llinks,Ilinks,Mlinks);
 % Ml_= Mleg;%kg
@@ -24,8 +26,8 @@ Ilinks = [465.732e-6,2350.113e-6];
 % 机体参数
 Mb_= 19.919;%kg
 Pb_= 0.0315;%m
-Jbpitch_ = 424025.762e-6;%kg/m^2
-Jbyaw_ = 438071.894e-6;
+Jbpitch_ = (424025.762e-6);%kg/m^2
+Jbyaw_ = (438071.894e-6);
 
 % 重力参数
 g_ = 9.81;
